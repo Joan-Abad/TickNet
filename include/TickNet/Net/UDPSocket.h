c8616242463a59115967285549403132699e2caa
Joan Abad <j.abad.reynoso@gmail.com>
2026-04-02 12:47:12 +0200

    Added the basic send to and recieve functionality

--- include/TickNet/Net/UDPSocket.h
+++ include/TickNet/Net/UDPSocket.h
@@ -2,12 +2,16 @@
 #include "TickNet/Platform/PlatformTypes.h"
 #include "TickNet/Platform/SocketHandle.h"
 
+class FIpAddress;
+
 class FUdpSocket
 {
 public:
     bool Open(uint16 Port);
     void Close();
     bool IsOpen() const;
+    int32 SendTo(const uint8* Data, int32 DataSize, const FIpAddress& Address);
+    int32 ReceiveFrom(uint8* Buffer, int32 BufferSize, FIpAddress& OutAddress);
 
 private:
     FSocketHandle Handle;
--- src/Net/UDPSocket.cpp
+++ src/Net/UDPSocket.cpp
@@ -4,6 +4,8 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 
+#include "TickNet/Net/IpAddress.h"
+
 bool FUdpSocket::Open(uint16 Port)
 {
     if (IsOpen())
@@ -68,4 +70,84 @@ void FUdpSocket::Close()
 bool FUdpSocket::IsOpen() const
 {
     return Handle.IsValid();
+}
+
+int32 FUdpSocket::SendTo(const uint8* Data, int32 DataSize, const FIpAddress& Address)
+{
+    //Check if the socket is open and valid before sending any information. 
+    if (!IsOpen())
+    {
+        return -1;
+    }
+
+    if (Data == nullptr || DataSize <= 0)
+    {
+        return -1;
+    }
+
+    sockaddr_in SockAddress = {};
+    SockAddress.sin_family = AF_INET;
+    SockAddress.sin_addr.s_addr = htonl(Address.GetAddress());
+    SockAddress.sin_port = htons(Address.GetPort());
+
+    const SOCKET NativeSocket = static_cast<SOCKET>(Handle.Value);
+
+    const int Result = sendto(
+        NativeSocket,
+        reinterpret_cast<const char*>(Data),
+        DataSize,
+        0,
+        reinterpret_cast<const sockaddr*>(&SockAddress),
+        sizeof(SockAddress));
+
+    if (Result == SOCKET_ERROR)
+    {
+        return -1;
+    }
+
+    return static_cast<int32>(Result);
+}
+
+int32 FUdpSocket::ReceiveFrom(uint8* Buffer, int32 BufferSize, FIpAddress& OutAddress)
+{
+    if (!IsOpen())
+    {
+        return -1;
+    }
+
+    if (Buffer == nullptr || BufferSize <= 0)
+    {
+        return -1;
+    }
+
+    sockaddr_in FromAddress = {};
+    int FromAddressSize = sizeof(FromAddress);
+
+    const SOCKET NativeSocket = static_cast<SOCKET>(Handle.Value);
+
+    const int Result = recvfrom(
+        NativeSocket,
+        reinterpret_cast<char*>(Buffer),
+        BufferSize,
+        0,
+        reinterpret_cast<sockaddr*>(&FromAddress),
+        &FromAddressSize);
+
+    if (Result == SOCKET_ERROR)
+    {
+        const int ErrorCode = WSAGetLastError();
+
+        if (ErrorCode == WSAEWOULDBLOCK)
+        {
+            return 0;
+        }
+
+        return -1;
+    }
+
+    OutAddress = FIpAddress(
+        ntohl(FromAddress.sin_addr.s_addr),
+        ntohs(FromAddress.sin_port));
+
+    return static_cast<int32>(Result);
 }
